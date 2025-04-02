862896bee7915d96a72c98d079a1c5371a200fe4
raphael r caldas <raphael008@hotmail.com>
2025-04-01 21:44:39 -0300

    login responsivo

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -49,7 +49,7 @@ const ChangePasswordModal = ({ show, onClose, onSubmit, token }) => {
                   <input
                      {...register("newPassword", { required: true })}
                      type='password'
-                     className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                     className='mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
                   />
                </div>
                <div className='mb-4'>
@@ -59,7 +59,7 @@ const ChangePasswordModal = ({ show, onClose, onSubmit, token }) => {
                   <input
                      {...register("confirmPassword", { required: true })}
                      type='password'
-                     className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                     className='mt-1 block w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
                   />
                </div>
                <div className='flex justify-center'>
@@ -119,73 +119,75 @@ const LoginPage = () => {
    };
 
    return (
-      <div className='flex flex-row h-screen bg-gray-200'>
-         <div className='grid items-center justify-center w-1/2 text-center'>
-            <div className='flex flex-col gap-12 p-6 bg-white rounded-lg shadow-md'>
-               <span className='text-4xl font-bold'>
+      <div className='flex flex-col md:flex-row h-full md:h-screen bg-gradient-to-r from-gray-100 to-gray-300'>
+         {/* Esquerda */}
+         <div className='flex items-center justify-center w-full md:w-1/2 p-6 md:p-0 text-center'>
+            <div className='flex flex-col gap-6 p-8 bg-white rounded-lg shadow-lg w-full max-w-md'>
+               <span className='text-4xl font-bold text-gray-800'>
                   FAT
                   <span className='text-red-500'>CONTROL</span>
                </span>
-               <div className='grid text-gray-400'>
+               <div className='text-gray-500 flex flex-col'>
                   <span className='text-xl font-bold'>
                      1º/1º Grupo de Transporte
                   </span>
-                  <span className='text-base font-bold'>
+                  <span className='text-base font-semibold'>
                      Uma Equipe, um coração.
                   </span>
                </div>
             </div>
          </div>
-         <div className='grid items-center justify-center w-1/2'>
-            <div className='p-8 bg-red-400 rounded-lg shadow-lg w-96'>
-               <h2 className='text-center text-white uppercase'>Login</h2>
-               <form onSubmit={handleSubmit(handleLogin)}>
-                  <div className='px-4 mt-6'>
-                     <input
-                        {...register("username", {
-                           required: true,
-                        })}
-                        className='bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                        autoComplete='off'
-                        onKeyPress={(event) => validateOnlyNumber(event)}
-                        maxLength={7}
-                        minLength={7}
-                        placeholder='SARAM'
-                     />
-                  </div>
-                  <div className='px-4 mt-5'>
-                     <input
-                        {...register("password", {
-                           required: true,
-                        })}
-                        className='bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                        autoComplete='off'
-                        placeholder='SENHA'
-                        type='password'
-                     />
-                  </div>
-                  <div className='grid mt-5 justify-items-center'>
-                     <Button
-                        color='blue'
-                        disabled={isLoading}
-                        className='w-1/2'
-                        type='submit'
-                     >
-                        {isLoading ? (
-                           <>
-                              <Spinner
-                                 color='failure'
-                                 aria-label='Failure spinner example'
-                              />
-                           </>
-                        ) : (
-                           <span className='text-lg'>Entrar</span>
-                        )}
-                     </Button>
-                  </div>
+
+         {/* Direita */}
+         <div className='flex items-center justify-center w-full md:w-1/2 p-6 md:p-0'>
+            <div className='p-8 bg-red-500 h-72 rounded-lg shadow-lg w-full max-w-md'>
+               <h2 className='text-center text-white uppercase text-lg font-bold'>
+                  Login
+               </h2>
+               <form
+                  onSubmit={handleSubmit(handleLogin)}
+                  className='flex flex-col items-center justify-center gap-4 mt-6'
+               >
+                  <input
+                     {...register("username", {
+                        required: true,
+                     })}
+                     className='bg-gray-50 w-2/3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block max-w-xs p-2'
+                     autoComplete='off'
+                     onKeyPress={(event) => validateOnlyNumber(event)}
+                     maxLength={7}
+                     minLength={7}
+                     placeholder='SARAM'
+                  />
+                  <input
+                     {...register("password", {
+                        required: true,
+                     })}
+                     className='bg-gray-50 w-2/3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block max-w-xs p-2'
+                     autoComplete='off'
+                     placeholder='SENHA'
+                     type='password'
+                  />
+                  <Button
+                     color='light'
+                     disabled={isLoading}
+                     className='w-2/3 max-w-xs bg-blue-500 enabled:hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md'
+                     type='submit'
+                  >
+                     {isLoading ? (
+                        <Spinner
+                           color='failure'
+                           aria-label='Failure spinner example'
+                        />
+                     ) : (
+                        <span className='text-sm'>Entrar</span>
+                     )}
+                  </Button>
                </form>
             </div>
          </div>
+
+         {/* Modal de Alteração de Senha */}
          <ChangePasswordModal
             show={showChangePasswordModal}
             onClose={() => setShowChangePasswordModal(false)}
