9c7e2715de183c505ccc191b5c648b6271f9f849
raphael r caldas <raphael008@hotmail.com>
2025-08-08 10:09:41 -0300

    inserindo mais informção nos cards de missão

--- src/app/(home)/cegep/missoes/registros/components/cardMission.tsx
+++ src/app/(home)/cegep/missoes/registros/components/cardMission.tsx
@@ -1,7 +1,8 @@
-import { Badge, Card, Tooltip } from "flowbite-react";
+import { Badge, Tooltip } from "flowbite-react";
 import { useState } from "react";
 import MissionDetail from "./missionDetail";
 import { Missao } from "services/routes/cegep/missoes";
+import clsx from "clsx";
 
 export function CardMission({
    missao,
@@ -16,11 +17,15 @@ export function CardMission({
       year: "2-digit",
       month: "2-digit",
       day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
    });
    const fim = new Date(missao.regres).toLocaleDateString("pt-BR", {
       year: "2-digit",
       month: "2-digit",
       day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
    });
 
    let colorBadge: string;
@@ -38,28 +43,65 @@ export function CardMission({
 
    return (
       <>
-         <Card
-            className='uppercase max-w-96 hover:shadow-lg cursor-pointer transition-shadow duration-300 ease-in-out'
+         <div
+            className='bg-white p-4 pt-6 shadow-md flex flex-col gap-3 rounded-lg uppercase max-w-96 hover:shadow-lg cursor-pointer transition-shadow duration-300 ease-in-out'
             onClick={() => setShowDetail(true)}
          >
-            <div className='flex justify-between mt-2'>
+            <div className='flex flex-row justify-between'>
                <Badge color={colorBadge}>{missao.tipo}</Badge>
-
-               {missao.indenizavel && (
-                  <Tooltip className='capitalize' content='Indenizável'>
-                     <Badge color='lime'>IND</Badge>
-                  </Tooltip>
-               )}
+               <h3 className='text-center w-full font-bold'>
+                  {missao.tipo_doc} {missao.n_doc}
+               </h3>
+               <Tooltip className='capitalize' content='Indenizável'>
+                  <Badge color={missao.indenizavel ? "success" : ""}>
+                     {missao.indenizavel ? "IND" : " "}
+                  </Badge>
+               </Tooltip>
             </div>
-            <h3 className='text-center font-bold'>
-               {missao.tipo_doc} {missao.n_doc}
-            </h3>
+
             <h4 className='text-center text-sm'>{missao.desc}</h4>
 
-            <p className='text-center lowercase'>
-               {ini} a {fim}
-            </p>
-         </Card>
+            <div className='flex flex-col gap-1 capitalize'>
+               <div className='flex flex-row'>
+                  <span className='w-28'>Afastamento:</span>
+                  <span className='font-mono'>{ini}</span>
+               </div>
+               <div className='flex flex-row'>
+                  <span className='w-28'>Regresso:</span>
+                  <span className='font-mono'>{fim}</span>
+               </div>
+            </div>
+
+            <div className='flex flex-wrap gap-2'>
+               {missao.pernoites.map((pnt) => {
+                  return (
+                     <span className='bg-slate-200 p-1 rounded-lg'>
+                        {pnt.cidade.nome}-{pnt.cidade.uf}
+                     </span>
+                  );
+               })}
+            </div>
+
+            <div className='flex gap-1 flex-wrap'>
+               {missao.users.map((user) => {
+                  return (
+                     <span
+                        key={user.id}
+                        className={clsx(
+                           "px-2.5 rounded-lg select-none font-medium",
+                           {
+                              "bg-blue-200": user.sit === "c",
+                              "bg-green-200": user.sit === "d",
+                              "bg-orange-200": user.sit === "g",
+                           }
+                        )}
+                     >
+                        {user.sit} | {user.p_g} {user.user.nome_guerra}
+                     </span>
+                  );
+               })}
+            </div>
+         </div>
 
          {showDetail && (
             <MissionDetail
