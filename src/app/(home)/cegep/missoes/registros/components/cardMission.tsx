3da03efac2447a8073d618d656bb1cbdf438454d
Raphael R Caldas <raphael008@hotmail.com>
2025-09-16 14:22:06 -0300

    refactor: modularizar componentes CardMis e PernoiteCardMis para melhorar a legibilidade e reutilização do código

--- src/app/(home)/cegep/missoes/registros/components/cardMission.tsx
+++ src/app/(home)/cegep/missoes/registros/components/cardMission.tsx
@@ -1,4 +1,4 @@
-import { Dropdown, DropdownItem } from "flowbite-react";
+import { Dropdown, DropdownItem, Tooltip } from "flowbite-react";
 import { useState, Dispatch, SetStateAction } from "react";
 import { FaRegClone } from "react-icons/fa";
 import MissionDetail from "./missionDetail";
@@ -17,34 +17,28 @@ export function CardMission({
    setShowForm: Dispatch<SetStateAction<boolean>>;
 }) {
    const [showDetail, setShowDetail] = useState(false);
+   const dayExt = {
+      day: "2-digit" as const,
+      month: "2-digit" as const,
+      year: "2-digit" as const,
+      hour: "2-digit" as const,
+      minute: "2-digit" as const,
+   };
+   const ini = new Date(missao.afast).toLocaleDateString("pt-BR", dayExt);
+   const fim = new Date(missao.regres).toLocaleDateString("pt-BR", dayExt);
 
-   const ini = new Date(missao.afast).toLocaleDateString("pt-BR", {
-      year: "2-digit",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-   });
-   const fim = new Date(missao.regres).toLocaleDateString("pt-BR", {
-      year: "2-digit",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-   });
-
-   let colorBadge: string;
-   switch (missao.tipo) {
-      case "adm":
-         colorBadge = "info";
-         break;
-      case "opr":
-         colorBadge = "warning";
-         break;
-      case "tal":
-         colorBadge = "success";
-         break;
-   }
+   // let colorBadge: string;
+   // switch (missao.tipo) {
+   //    case "adm":
+   //       colorBadge = "info";
+   //       break;
+   //    case "opr":
+   //       colorBadge = "warning";
+   //       break;
+   //    case "tal":
+   //       colorBadge = "success";
+   //       break;
+   // }
 
    function onClone() {
       const clone = { ...missao };
@@ -88,36 +82,15 @@ export function CardMission({
                </div>
 
                <div className='flex flex-col gap-2'>
-                  {missao.pernoites.map((pnt) => {
-                     return (
-                        <div key={pnt.id}>
-                           
-                           <span className='bg-slate-200 py-1 px-2 rounded-lg'>
-                              {pnt.cidade.nome}-{pnt.cidade.uf}
-                           </span>
-                        </div>
-                     );
-                  })}
+                  {missao.pernoites.map((pnt) => (
+                     <PernoiteCardMis key={pnt.id} pnt={pnt} />
+                  ))}
                </div>
 
                <div className='flex gap-1 flex-wrap'>
-                  {missao.users.map((user) => {
-                     return (
-                        <span
-                           key={user.id}
-                           className={clsx(
-                              "px-2.5 rounded-lg select-none font-medium",
-                              {
-                                 "bg-blue-200": user.sit === "c",
-                                 "bg-green-200": user.sit === "d",
-                                 "bg-orange-200": user.sit === "g",
-                              }
-                           )}
-                        >
-                           {user.sit} | {user.p_g} {user.user.nome_guerra}
-                        </span>
-                     );
-                  })}
+                  {missao.users.map((user) => (
+                     <UserCardMis key={user.id} user={user} />
+                  ))}
                </div>
             </div>
          </div>
@@ -133,5 +106,57 @@ export function CardMission({
             />
          )}
       </>
+   );
+}
+
+function PernoiteCardMis({ pnt }) {
+   const dayExt = {
+      day: "2-digit" as const,
+      month: "2-digit" as const,
+      year: "2-digit" as const,
+   };
+   const dataIni = new Date(pnt.data_ini).toLocaleDateString("pt-br", dayExt);
+   const dataFim = new Date(pnt.data_fim).toLocaleDateString("pt-br", dayExt);
+
+   return (
+      <div className='p-1 flex flex-row gap-1 rounded-lg bg-gray-200 items-center justify-start text-sm'>
+         <span className='w-40 font-mono lowercase text-center'>
+            {dataIni} a {dataFim}
+         </span>
+         <span className='font-medium'>
+            {pnt.cidade.nome}-{pnt.cidade.uf}
+         </span>
+         <div className='flex flex-row gap-0.5'>
+            {pnt.acrec_desloc && (
+               <Tooltip content='Acréscimo Deslocamento'>
+                  <span className='font-semibold bg-green-400 px-2 rounded-lg'>
+                     AC
+                  </span>
+               </Tooltip>
+            )}
+
+            {pnt.meia_diaria && (
+               <Tooltip content='Meia Diária'>
+                  <span className='font-semibold bg-amber-400 px-2 rounded-lg'>
+                     MD
+                  </span>
+               </Tooltip>
+            )}
+         </div>
+      </div>
+   );
+}
+
+function UserCardMis({ user }) {
+   return (
+      <span
+         className={clsx("px-2.5 rounded-lg select-none font-medium", {
+            "bg-blue-200": user.sit === "c",
+            "bg-green-200": user.sit === "d",
+            "bg-orange-200": user.sit === "g",
+         })}
+      >
+         {user.sit} | {user.p_g} {user.user.nome_guerra}
+      </span>
    );
 }
